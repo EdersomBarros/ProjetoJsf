d5f47bc18db1e3d5597683f6e4dbfd3d364d8e14
Edersom Barros <edersombarros@hotmail.com>
2022-12-11 09:26:32 -0400

    projeto inicial

---
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -0,0 +1,38 @@
+package br.com.projetojsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+
+	private String nome;
+
+	private List<String> nomes = new ArrayList<String>();
+
+	public String addNome() {
+		nomes.add(nome);
+		return "";
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
