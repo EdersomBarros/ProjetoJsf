8954b62f4d5d4a0df64237d828e29ef9582c3c4e
Edersom Barros <edersombarros@hotmail.com>
2022-12-11 23:10:09 -0400

    SelectManyMenu em JSF

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -12,63 +12,81 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-public class Pessoa implements Serializable{
-	
-	
+public class Pessoa implements Serializable {
+
 	private static final long serialVersionUID = 6210133012248387311L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String nome;
 	private String sobrenome;
 	private Integer idade;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
-	
+
 	private String sexo;
+	private String[] frameworks;
 	
-	
+
+	public String[] getFrameworks() {
+		return frameworks;
+	}
+
+	public void setFrameworks(String[] frameworks) {
+		this.frameworks = frameworks;
+	}
+
 	public String getSexo() {
 		return sexo;
 	}
-	
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+
 	public Pessoa() {
-		
+
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 	public Integer getIdade() {
 		return idade;
 	}
+
 	public void setIdade(Integer idade) {
 		this.idade = idade;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
@@ -89,10 +107,5 @@ public class Pessoa implements Serializable{
 		Pessoa other = (Pessoa) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
 
 }
