740bfd1e1024de924bb7d93a4d222ab61cc1921c
Edersom Barros <edersombarros@hotmail.com>
2022-12-28 22:36:59 -0400

    Criando relatório

---
+++ src/main/java/br/com/projetojsf/RelLancamento.java
@@ -0,0 +1,41 @@
+package br.com.projetojsf;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.dao.DaoGeneric;
+import br.com.entidades.Lancamento;
+import br.com.repository.IDaoLancamento;
+
+@ViewScoped
+@Named(value = "relLancamento")
+public class RelLancamento implements Serializable{
+
+	private static final long serialVersionUID = -725502281632649908L;
+	
+	private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	@Inject
+	private IDaoLancamento iDaoLancamento;
+	@Inject
+	private DaoGeneric<Lancamento> daoGeneric;
+	
+	public void buscarLancamento() {
+		lancamentos  = daoGeneric.getListEntity(Lancamento.class);
+	}
+	
+	
+	
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+	
+
+}
